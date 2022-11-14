132ea3cdb3e4392d3272b025ac53aa90db3a8df1
云知易客 <1647762341@qq.com>
2022-11-14 14:59:58 +0800

    composer修改

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ docker-compose up -d
 echo -e "\033[32m 4.删除缓存 \033[0m"
 for i in `ls /www/wwwroot/logs/`;
 do
+ echo -e "\033[41;30m [删除] /www/wwwroot/logs/$i/container \033[0m"
  rm -rf /www/wwwroot/logs/$i/container;
 done
-echo -e "\033[32m 5.【服务部署完成 SUCCESS】 \033[0m"
+echo -e "\033[32m 【服务部署完成 SUCCESS】 \033[0m"

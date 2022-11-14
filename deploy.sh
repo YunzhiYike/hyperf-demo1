1256ddef7c76c48b0669712ccfef80f01a904a67
云知易客 <1647762341@qq.com>
2022-11-14 14:54:37 +0800

    composer修改

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-# 删除停止的容器
+#
+echo -e "\033[32m 1.删除停止的容器 \033[0m"
 docker rm `docker ps -a|grep Exited|awk '{print $1}'`
-# 删除所有镜像
+echo -e "\033[32m 2.删除所有docker镜像 \033[0m"
 sudo docker rmi $(docker images -q)
-# 执行docker-compose
+echo -e "\033[32m 3.执行docker-compose \033[0m"
 docker-compose up -d
-# 删除缓存
+echo -e "\033[32m 4.删除缓存 \033[0m"
 for i in `ls /www/wwwroot/logs/`;
 do
  rm -rf /www/wwwroot/logs/$i/container;
 done
+echo -e "\033[32m 【服务部署完成 SUCCESS】 \033[0m"
